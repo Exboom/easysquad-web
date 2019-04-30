368820048547d5a5b996cc146337c03851d0af4d
Andrey Popov <apo@aiweapps.com>
2019-04-30 13:56:40 +0300

    Chekin

--- app/models/chekin.rb
+++ app/models/chekin.rb
@@ -2,4 +2,6 @@ class Chekin < ApplicationRecord
 
   validates :chekin,
             :inclusion => { :in => [true, false] }
+  validates :presence,
+            :inclusion => { :in => [true, false] }
 end
