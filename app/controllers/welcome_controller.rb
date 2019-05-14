3505165bc51d356cc049fd45139e6698a7427b96
Andrey Popov <apo@aiweapps.com>
2019-05-14 11:28:36 +0300

    Registration confirmation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,19 +14,19 @@ class WelcomeController < ApplicationController
     @locs=Location.all
     @users=User.all
 
-    # if current_user!=nil
-    #   param=UserRole.find_by user1: current_user.id
-    #   @userrols = Role.find(param.role1)
-    # end
-    #
-    # if (current_user!=nil) and (@userrols.id!=1)
-    #   search=Team.find(PlayerTeam.where("player = ?", current_user.id).pluck(:team))
-    #   @nextgame = Game.where("(team_one = ? OR team_two = ?) AND game_score is NULL", search, search).take
-    #   if @nextgame.nil?
-    #   else
-    #     @app=Application.find_by player:current_user.id, tournament: @nextgame.tournament
-    #   end
-    # end
+    if current_user!=nil
+      param=UserRole.find_by user1: current_user.id
+      @userrols = Role.find(param.role1)
+    end
+
+    if (current_user!=nil) and (@userrols.id!=1)
+      search=Team.find(PlayerTeam.where("player = ?", current_user.id).pluck(:team))
+      @nextgame = Game.where("(team_one = ? OR team_two = ?) AND game_score is NULL", search, search).take
+      if @nextgame.nil?
+      else
+        @app=Application.find_by player:current_user.id, tournament: @nextgame.tournament
+      end
+    end
 
   end
 
