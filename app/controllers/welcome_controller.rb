7a59b146d0fa558f1b188f1c80440fee66bded1f
Andrey Popov <apo@aiweapps.com>
2019-05-14 10:43:57 +0300

    UI

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,19 +14,19 @@ class WelcomeController < ApplicationController
     @locs=Location.all
     @users=User.all
 
-    if current_user!=nil
-      param=UserRole.find_by user1: current_user.id
-      @userrols = Role.find(param.role1)
-    end
-
-    if (current_user!=nil) and (@userrols.id!=1)
-      search=Team.find(PlayerTeam.where("player = ?", current_user.id).pluck(:team))
-      @nextgame = Game.where("(team_one = ? OR team_two = ?) AND game_score is NULL", search, search).take
-      if @nextgame.nil?
-      else
-        @app=Application.find_by player:current_user.id, tournament: @nextgame.tournament
-      end
-    end
+    # if current_user!=nil
+    #   param=UserRole.find_by user1: current_user.id
+    #   @userrols = Role.find(param.role1)
+    # end
+    #
+    # if (current_user!=nil) and (@userrols.id!=1)
+    #   search=Team.find(PlayerTeam.where("player = ?", current_user.id).pluck(:team))
+    #   @nextgame = Game.where("(team_one = ? OR team_two = ?) AND game_score is NULL", search, search).take
+    #   if @nextgame.nil?
+    #   else
+    #     @app=Application.find_by player:current_user.id, tournament: @nextgame.tournament
+    #   end
+    # end
 
   end
 
@@ -42,10 +42,23 @@ class WelcomeController < ApplicationController
 
   end
 
+  protected
+
   def check_input
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+
+    # if user_signed_in?
+    #   @user=User.find(current_user.id)
+    #   redirect_to new_user_session_path, alert:  "Ваша учетная запись еще не активирована" if @user.approved==false
+    # end
+
   end
+
+  # def check_approved
+  #   @user=User.find(current_user.id)
+  #   redirect_to new_user_session_path, alert:  "Ваша учетная запись еще не активирована" if @user.approved==false
+  # end
 
 end
