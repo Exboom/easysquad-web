dbf62b42bedb832a1b6779262d958e7fe527eaeb
Andrey Popov <apo@aiweapps.com>
2019-06-21 14:49:43 +0300

    Added the ability to view past games player. Display username instead of Email. Displaying the user's role in the command

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,53 +9,12 @@ class WelcomeController < ApplicationController
         @player = Player.find(current_user.id)
         @teampl=@player.teams
         @nextgames=Array.new(@teampl.size)
+        @lastgames=Array.new(@teampl.size)
         @teampl.each_with_index do |teampl, index|
           @nextgames[index]=Game.where("(team_one = ? OR team_two = ?) AND game_score is NULL", teampl, teampl)
+          @lastgames[index]=Game.where("(team_one = ? OR team_two = ?) AND game_score is NOT NULL", teampl, teampl)
         end
 
-        if @nextgames.to_s=="[nil]"
-          puts "она ноль"
-        else
-            puts "вывод числа элементов nextgames "+@nextgames.size.to_s
-            puts "Это вывод @nextgames 1 "+@nextgames[0].to_s
-            puts "Это вывод @nextgames 2 "+@nextgames[1].to_s
-
-
-
-            @nextgames.each do |nxt|
-              nxt.each do |nxtg|
-                puts nxtg.name
-                appl=Application.find_by player:@player, tournament: nxtg.tournament
-                puts "Это номер заявки "+appl.id.to_s
-                chek= Chekin.find_by player:@player, game: nxtg
-                if chek.nil?
-                  puts "Такого чекина нет"
-                else
-                  puts "Это номер чекина "+chek.id.to_s
-                end
-
-              end
-            end
-
-
-            # @app=Array.new(@nextgames.size)
-            # @lastchekin=Array.new(@nextgames.size)
-            #
-            # @nextgames.each_with_index do |nxtgmmas, index1|
-            #   nxtgmmas[index1].each_with_index do |nxtgm, index2|
-            #     @app[index2]=Application.find_by player:current_user.id, tournament: nxtgm.tournament
-            #     @lastchekin[index2]=Chekin.find_by player:@player, game: @nextgame[index2]
-            #   end
-            # end
-            #
-            #
-            # @nextgame.each_with_index do |nxtgm, index|
-            #   @app[index]=Application.find_by player:current_user.id, tournament: nxtgm.tournament
-            #   @lastchekin[index]=Chekin.find_by player:@player, game: @nextgame[index]
-            # end
-
-
-        end
 
       elsif @userrols.find_by_id(2)!=nil
 
